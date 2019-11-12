31c1d7041dfbb404f90eb66fe0f29c7289329a3e
phong <phong_pham88@hotmail.com>
2019-11-12 02:50:10 -0700

    Added axios to the project in order to recieve all current pots

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import Scream from "../components/Scream";
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-            </div>
-        )
-    }
+  state = {
+    screams: null
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
