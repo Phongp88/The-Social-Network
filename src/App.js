d847d7bed49e750ad92960798b52bf9c5feaf390
phong <phong_pham88@hotmail.com>
2019-11-12 01:49:43 -0700

    Added theme whole page to keep everything consistent

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 // Pages
-import home from './pages/home'
-import login from './pages/login'
-import signup from './pages/signup'
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 // Components
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffca28",
+      contrastText: "#fff",
+      light: '#ffd453',
+      dark: '#b28d1c',
+    },
+    secondary: {
+      main: "#fafafa",
+      contrastText: "#fff",
+      light: '#fbfbfb',
+      dark: '#afafaf'
+    }
+  }
+});
 function App() {
   return (
     <div className="App">
-      <Router>
-      <NavBar></NavBar>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home}/>
-            <Route exact path="/login" component={login}/>
-            <Route exact path="/signup" component={signup}/>
-          </Switch>
-        </div>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <NavBar></NavBar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     </div>
   );
 }
