abf8c62dd1f92e7042b00c01d22ee8ec517082ed
phong <phong_pham88@hotmail.com>
2019-11-13 19:23:31 -0700

    user may signup and authenticate user inputs with redux

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,13 +1,158 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/icon.PNG";
+import { Link } from "react-router-dom";
+// MUI ITEMS
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Typepography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+
+// Redux
+import {connect} from 'react-redux'
+import {signupUser} from '../redux/actions/userAction'
+const styles = theme => ({
+  ...theme.frontPage
+});
 
 export class signup extends Component {
-    render() {
-        return (
-            <div>
-                <h1>signup page</h1>
-            </div>
-        )
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      handle: "",
+      loading: false,
+      errors: {}
+    };
+  }
+  componentWillReceiveProps(nextProps){
+    if(nextProps.UI.errors){
+      this.setState({errors: nextProps.UI.errors})
     }
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      handle: this.state.handle
+    };
+    this.props.signupUser(newUserData, this.props.history);
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  render() {
+    const { classes, UI: {loading} } = this.props;
+    const { errors } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="Corgi" className={classes.image} />
+          <Typepography variant="h3" className={classes.pageTitle}>
+            Signup
+          </Typepography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              className={classes.textField}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="handle"
+              name="handle"
+              type="text"
+              label="Handle"
+              helperText={errors.handle}
+              error={errors.handle ? true : false}
+              className={classes.textField}
+              value={this.state.handle}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            {errors.general && (
+              <Typepography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typepography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+              className={classes.button}
+            >
+              Signup
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Already have an account? login up <Link to="/login">here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
-export default signup
+signup.propTypes = {
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI
+})
+
+
+export default connect(mapStateToProps, {signupUser})(withStyles(styles)(signup));
