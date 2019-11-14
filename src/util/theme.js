da6d1c25d2b43c6a5ce46c6d0d344d7fc182e769
phong <phong_pham88@hotmail.com>
2019-11-14 16:18:16 -0700

    fixed indentation for theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -1,92 +1,92 @@
 export default {
-    palette: {
-      primary: {
-        main: "#ffca28",
-        contrastText: "#fff",
-        light: "#ffd453",
-        dark: "#b28d1c"
-      },
-      secondary: {
-        main: "#fafafa",
-        contrastText: "black",
-        light: "#fbfbfb",
-        dark: "#afafaf"
-      }
+  palette: {
+    primary: {
+      main: "#ffca28",
+      contrastText: "#fff",
+      light: "#ffd453",
+      dark: "#b28d1c"
     },
-    typography: {
-      useNextVariants: true
+    secondary: {
+      main: "#fafafa",
+      contrastText: "black",
+      light: "#fbfbfb",
+      dark: "#afafaf"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  frontPage: {
+    form: {
+      textAlign: "center"
     },
-    frontPage: {
-      form: {
-        textAlign: "center"
-      },
-      image: {
-        margin: "20px auto 20px auto"
-      },
-      pageTitle: {
-        margin: "10px auto 10px auto"
-      },
-      textField: {
-        margin: "10px auto 10px auto"
-      },
-      button: {
-        marginTop: 20,
-        position: "relative"
-      },
-      customError: {
-        color: "red",
-        fontSize: "0.8rem",
-        marginTop: 10
-      },
-      progress: {
-        position: "absolute"
-      }
+    image: {
+      margin: "20px auto 20px auto"
     },
+    pageTitle: {
+      margin: "10px auto 10px auto"
+    },
+    textField: {
+      margin: "10px auto 10px auto"
+    },
+    button: {
+      marginTop: 20,
+      position: "relative"
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10
+    },
+    progress: {
+      position: "absolute"
+    }
+  },
   userProfile: {
     profile: {
-      '& .image-wrapper': {
-        textAlign: 'center',
-        position: 'relative',
-        '& button': {
-          position: 'absolute',
-          top: '80%',
-          left: '70%'
+      "& .image-wrapper": {
+        textAlign: "center",
+        position: "relative",
+        "& button": {
+          position: "absolute",
+          top: "80%",
+          left: "70%"
         }
       },
-      '& .profile-image': {
+      "& .profile-image": {
         width: 200,
         height: 200,
-        objectFit: 'cover',
-        maxWidth: '100%',
-        borderRadius: '50%'
+        objectFit: "cover",
+        maxWidth: "100%",
+        borderRadius: "50%"
       },
-      '& .profile-details': {
-        textAlign: 'center',
-        '& span, svg': {
-          verticalAlign: 'middle'
+      "& .profile-details": {
+        textAlign: "center",
+        "& span, svg": {
+          verticalAlign: "middle"
         },
-        '& a': {
-          color: '#00bcd4'
+        "& a": {
+          color: "#00bcd4"
         }
       },
-      '& hr': {
-        border: 'none',
-        margin: '0 0 10px 0'
+      "& hr": {
+        border: "none",
+        margin: "0 0 10px 0"
       },
-      '& svg.button': {
-        '&:hover': {
-          cursor: 'pointer'
+      "& svg.button": {
+        "&:hover": {
+          cursor: "pointer"
         }
       }
     },
     buttons: {
-      textAlign: 'center',
-      '& a': {
-        margin: '20px 10px'
+      textAlign: "center",
+      "& a": {
+        margin: "20px 10px"
       }
     },
     paper: {
       padding: 20
     }
   }
-  }
+};
