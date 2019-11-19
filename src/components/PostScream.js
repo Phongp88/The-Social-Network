f95a405f835061fdc3eb7fd6ed4f5afbe8dce29c
phong <phong_pham88@hotmail.com>
2019-11-18 18:06:24 -0700

    created post component that handles user posts

---
+++ src/components/PostScream.js
@@ -0,0 +1,144 @@
+import React, { Component, Fragment } from "react";
+import Proptypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// MUI
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
+// Icons
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
+// Redux
+import { connect } from "react-redux";
+import { postScream } from "../redux/actions/dataAction";
+import MyButton from "../util/MyButton";
+
+const styles = theme => ({
+  ...theme.frontPage,
+  submitButton: {
+    position: "relative"
+  },
+  progressSpinner: {
+    position: "absolute"
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%",
+    top: "10%"
+  },
+  dialogBox: {
+      paddingBottom: 15
+  }
+});
+
+class PostScream extends Component {
+  state = {
+    open: false,
+    body: "",
+    errors: {}
+  };
+  componentWillReceiveProps(nextProps){
+      if(nextProps.UI.errors){
+          this.setState({
+              errors: nextProps.UI.errors
+          })
+      }
+      if(!nextProps.UI.errors && !nextProps.UI.loading) {
+          this.setState({body: ""});
+          this.handleClose();
+      }
+  }
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+  handleClose = () => {
+    this.setState({ open: false, errors: {} });
+  };
+  handleChange = (event) => {
+      this.setState({[event.target.name]: event.target.value})
+  }
+  handleSubmit = (event) => {
+      event.preventDefault();
+      this.props.postScream({body: this.state.body});
+  }
+  render() {
+    const { errors } = this.state;
+    const {
+      classes,
+      UI: { loading }
+    } = this.props;
+    return (
+      <Fragment>
+        <MyButton onClick={this.handleOpen} tip="Post a Bark!">
+          <AddIcon />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <MyButton
+            tip="Close"
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogTitle>Post a new bark</DialogTitle>
+          <DialogContent className={classes.dialogBox}>
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                name="body"
+                type="text"
+                label="Post"
+                multiline
+                rows="3"
+                placeholder="What's on your mind?"
+                error={errors.body ? true : false}
+                helperText={errors.body}
+                className={classes.textField}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                disabled={loading}
+              >
+                Submit
+                {loading && (
+                  <CircularProgress
+                    size={30}
+                    className={classes.progressSpinner}
+                  />
+                )}
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
+
+PostScream.propTypes = {
+  postScream: Proptypes.func.isRequired,
+  UI: Proptypes.object.isRequired,
+  classes: Proptypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  UI: state.UI
+});
+
+export default connect(mapStateToProps, { postScream })(
+  withStyles(styles)(PostScream)
+);
