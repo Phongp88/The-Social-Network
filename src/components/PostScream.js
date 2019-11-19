49691e868233d5afe3b73039a6c332c29d65659e
phong <phong_pham88@hotmail.com>
2019-11-18 20:27:57 -0700

    changed import placement for organization

--- src/components/PostScream.js
+++ src/components/PostScream.js
@@ -1,11 +1,10 @@
 import React, { Component, Fragment } from "react";
 import Proptypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
-
+import MyButton from "../util/MyButton";
 // MUI
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
@@ -16,7 +15,7 @@ import CloseIcon from "@material-ui/icons/Close";
 // Redux
 import { connect } from "react-redux";
 import { postScream, clearErrors } from "../redux/actions/dataAction";
-import MyButton from "../util/MyButton";
+
 
 const styles = theme => ({
   ...theme.frontPage,
