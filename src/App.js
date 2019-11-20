478bc684023a12939252aa99d31ee35b9dd94b95
phong <phong_pham88@hotmail.com>
2019-11-19 20:06:34 -0700

    added user profile page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {logoutUser, getUserData} from './redux/actions/userAction'
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import user from "./pages/user"
 // Components
 import NavBar from "./components/layout/NavBar";
 import AuthRoute from "./util/AuthRoute";
@@ -55,6 +56,7 @@ function App() {
                   path="/signup"
                   component={signup}
                 />
+                <Route exact path="/users/:handle" component={user}/>
               </Switch>
             </div>
           </Router>
