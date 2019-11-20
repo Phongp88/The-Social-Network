96e163ec6deb3641fe8d8c7c77f45ddabadd35d4
phong <phong_pham88@hotmail.com>
2019-11-19 18:56:34 -0700

    added seperator css to global theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -43,6 +43,15 @@ export default {
     }
   },
   userProfile: {
+    invisibleSeperator: {
+      border: "none",
+      margin: 4
+    },
+    visibleSeperator: {
+      width: "100%",
+      borderBottom: "1px solid rgba(0,0,0,0.1)",
+      marginBottom: 20
+    },
     profile: {
       "& .image-wrapper": {
         textAlign: "center",
