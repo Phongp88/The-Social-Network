2270ef24c01cfaa5c87b47aed297ff9a18c1cc37
phong <phong_pham88@hotmail.com>
2019-11-21 01:22:00 -0700

    added default url for axios

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import jwtDecode from "jwt-decode";
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import {SET_AUTHENTICATED} from './redux/types'
-import {logoutUser, getUserData} from './redux/actions/userAction'
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userAction";
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
-import user from "./pages/user"
+import user from "./pages/user";
 // Components
 import NavBar from "./components/layout/NavBar";
 import AuthRoute from "./util/AuthRoute";
 import axios from "axios";
+
+axios.defaults.baseUrl =
+  "https://us-central1-social-media-network-41bea.cloudfunctions.net/api";
 
 const theme = createMuiTheme(themeFile);
 
@@ -28,41 +31,37 @@ if (token) {
   const decodeToken = jwtDecode(token);
   // grabs expirey date of token
   if (decodeToken.exp * 1000 < Date.now()) {
-    store.dispatch(logoutUser())
+    store.dispatch(logoutUser());
     window.location.href = "/login";
   } else {
-    store.dispatch({type: SET_AUTHENTICATED});
-    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
   }
 }
 
 function App() {
   return (
-      <MuiThemeProvider theme={theme}>
-        <Provider store={store}>
-          <Router>
-            <NavBar></NavBar>
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={home} />
-                <AuthRoute
-                  exact
-                  path="/login"
-                  component={login}
-                />
-                <AuthRoute
-                  exact
-                  path="/signup"
-                  component={signup}
-                />
-                <Route exact path="/users/:handle" component={user}/>
-                <Route exact path="/users/:handle/scream/:screamId" component={user}/>
-              </Switch>
-            </div>
-          </Router>
-        </Provider>
-      </MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Router>
+          <NavBar></NavBar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
+              <Route exact path="/users/:handle" component={user} />
+              <Route
+                exact
+                path="/users/:handle/scream/:screamId"
+                component={user}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    </MuiThemeProvider>
   );
 }
 
